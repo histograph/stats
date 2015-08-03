2c540a37ed6d0b91ceeeb5345df210a10f53026e
Bert Spaan <bert.spaan@gmail.com>
2015-08-03 13:23:46 +0200

    single queue!

--- index.js
+++ index.js
@@ -3,31 +3,17 @@ var express = require('express');
 var router = express.Router();
 var config = require('histograph-config');
 var client = redis.createClient(config.redis.port, config.redis.host);
-var queues = config.redis.queues;
-var async = require('async');
 
-router.get('/queues', function(req, res) {
-  async.map(Object.keys(queues), function(queueId, callback) {
-    client.llen(queues[queueId], function(err, reply) {
-      if (err) {
-        callback(err);
-      }  else {
-        callback(null, {
-          redisName: queues[queueId],
-          queue: queueId,
-          length: reply
-        });
-      }
-    });
-  },
-
-  function(err, lengths) {
+router.get('/queue', function(req, res) {
+  client.llen(config.redis.queue, function(err, reply) {
     if (err) {
       res.status(400).send({
         message: err
       });
     } else {
-      res.send(lengths);
+      return {
+        length: reply
+      };
     }
   });
 });
